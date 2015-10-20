47570463f743224ca007c84b5340242e3ba42d5d
jiayaoaaa <jia_yao@eventown.com.cn>
2015-10-20 19:35:06 +0800

    init

--- models/photo.js
+++ models/photo.js
@@ -1,1 +1,16 @@
-//文章模型
+//图片模型
+
+
+var mongoose  = require('mongoose');
+var Schema    = mongoose.Schema;
+var PhotoSchema = new Schema({
+  title: { type: String },
+  discrib:{type:String},
+  author_id: { type: String},
+  content:{type:String},
+  create_at: { type: Date, default: Date.now },
+  update_at: { type: Date, default: Date.now },
+
+});
+// AticleSchema.index({title: 1}, {unique: true});
+mongoose.model('Photo', PhotoSchema);
