07d21b990243867b4e5883ff524a199b481b3a0a
wanglei8381 <wanglei8381@163.com>
2016-01-06 14:45:24 +0800

    set views directory

--- app.js
+++ app.js
@@ -12,32 +12,33 @@ var app = express();
 var cors = require('cors');
 
 var handlebars = require('express-handlebars')
-    .create({
-        defaultLayout: 'main',
-        helpers: {
-            section: function(name, options) {
-                if (!this._sections) this._sections = {};
-                this._sections[name] = options.fn(this);
-                return null;
-            }
-        }
-    });
+  .create({
+    defaultLayout: 'main',
+    helpers: {
+      section: function (name, options) {
+        if (!this._sections) this._sections = {};
+        this._sections[name] = options.fn(this);
+        return null;
+      }
+    }
+  });
 
 // view engine setup
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
 
 // //开启cookie
 app.use(cookieParser(config.session_secret));
 app.use(session({
-    secret: config.session_secret,
-    name: 'defautsession',
+  secret: config.session_secret,
+  name: 'defautsession',
 
-    cookie: {
-        maxAge: 1000 * 60 * 30
-    }, //session设置30分钟
-    resave: false,
-    saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 30
+  }, //session设置30分钟
+  resave: false,
+  saveUninitialized: true,
 }))
 
 
@@ -48,7 +49,6 @@ app.use(session({
 // app.use(function(req, res, next) {
 
 // })
-
 
 
 //消息提示组件
@@ -64,33 +64,32 @@ app.use(session({
 // app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 
 
 //处理session的中间件
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 
 
-    req.user = res.locals.user = req.session.user;
-    next();
+  req.user = res.locals.user = req.session.user;
+  next();
 });
 
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 
 
-      if (!req.session.user) {
+  if (!req.session.user) {
 
-        console.log('no session');
-        var login=require('./controllers/sign.js');
-        login.checkIsLogin(req,res,next)
-        }else{
-            next()
-        }
+    console.log('no session');
+    var login = require('./controllers/sign.js');
+    login.checkIsLogin(req, res, next)
+  } else {
+    next()
+  }
 
 });
-
 
 
 //static中间件
@@ -115,41 +114,34 @@ app.use('/', router);
 // });
 
 
+app.get('/vacations', function (req, res) {
+  vacation.find({
+    available: true
+  }, function (err, vacations) {
 
-
-
-
-
-
-
-app.get('/vacations', function(req, res) {
-    vacation.find({
-        available: true
-    }, function(err, vacations) {
-
-        var context = {
-            vacations: vacations.map(function(vacation) {
-                return {
-                    sku: vacation.sku,
-                    name: vacation.name,
-                    descripton: vacation.descripton,
-                    price: vacation.getDisplayPrice(),
-                    inSeason: vacation.inSeason,
-                }
-            })
+    var context = {
+      vacations: vacations.map(function (vacation) {
+        return {
+          sku: vacation.sku,
+          name: vacation.name,
+          descripton: vacation.descripton,
+          price: vacation.getDisplayPrice(),
+          inSeason: vacation.inSeason,
         }
+      })
+    }
 
-    })
+  })
 
-    res.render('vacations', context)
+  res.render('vacations', context)
 
 })
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -157,38 +149,37 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
-var server = app.listen(config.port, function() {
-    var host = 'localhost';
-    var port = server.address().port;
+var server = app.listen(config.port, function () {
+  var host = 'localhost';
+  var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%s', host, port);
 });
 
 
 //socket 相关
 
 var users = require('./user_data').users;
-
 
 
 var io = require('socket.io').listen(server);
@@ -198,92 +189,85 @@ var usernames = {};
 var numUsers = 0;
 
 
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  io.emit('connect', usernames);
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+
+    if (addedUser) {
+      delete usernames[socket.username];
+      --numUsers;
+    }
 
 
-io.on('connection', function(socket) {
-    var addedUser = false;
-
-    io.emit('connect', usernames);
-
-    socket.on('disconnect', function() {
-        console.log('user disconnected');
-
-        if (addedUser) {
-            delete usernames[socket.username];
-            --numUsers;
-        }
+    socket.broadcast.emit('user left', {
+      username: socket.username,
+      numUsers: numUsers
+    });
 
 
-        socket.broadcast.emit('user left', {
-            username: socket.username,
-            numUsers: numUsers
+  });
+
+
+  socket.on('message', function (msg) {
+    io.emit('messagecome', msg);
+  });
+
+
+  socket.on('vote', function (name) {
+    users.forEach(function (value, index) {
+      if (value.name == name) {
+        value.score++;
+
+        io.emit('upmessagecome', {
+          name: value.name,
+          score: value.score
         });
+        return false
+
+      }
+    })
+  });
 
 
-
-
-    });
-
-
-
-
-    socket.on('message', function(msg) {
-        io.emit('messagecome', msg);
-    });
-
-
-    socket.on('vote', function(name) {
-        users.forEach(function(value, index) {
-            if (value.name == name) {
-                value.score++;
-
-                io.emit('upmessagecome', {
-                    name: value.name,
-                    score: value.score
-                });
-                return false
-
-            }
-        })
-    });
-
-
-        socket.on('down', function(name) {
-        users.forEach(function(value, index) {
-            if (value.name == name) {
-                value.score--;
-                io.emit('upmessagecome', {
-                    name: value.name,
-                    score: value.score
-                });
-                return false
-
-            }
-        })
-    });
-
-
-    socket.on('add user', function(username) {
-        console.log(username);
-        addedUser = true;
-        // we store the username in the socket session for this client
-        socket.username = username;
-        usernames[username] = username;
-        ++numUsers;
-        socket.broadcast.emit('login', {
-            numUsers: numUsers,
-            username: socket.username
+  socket.on('down', function (name) {
+    users.forEach(function (value, index) {
+      if (value.name == name) {
+        value.score--;
+        io.emit('upmessagecome', {
+          name: value.name,
+          score: value.score
         });
+        return false
 
-        // echo globally (all clients) that a person has connected
-        io.emit('user joined', {
-            usernames: usernames,
-            numUsers: numUsers,
-            voteUsers:users
-        });
+      }
+    })
+  });
 
+
+  socket.on('add user', function (username) {
+    console.log(username);
+    addedUser = true;
+    // we store the username in the socket session for this client
+    socket.username = username;
+    usernames[username] = username;
+    ++numUsers;
+    socket.broadcast.emit('login', {
+      numUsers: numUsers,
+      username: socket.username
     });
 
+    // echo globally (all clients) that a person has connected
+    io.emit('user joined', {
+      usernames: usernames,
+      numUsers: numUsers,
+      voteUsers: users
+    });
+
+  });
 
 
 });
