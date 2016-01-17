48baec102793efcc26770d9aca004913f6aef9cb
goodliang <253715900@qq.com>
2016-01-17 17:27:31 +0800

    up

--- config.js
+++ config.js
@@ -35,8 +35,8 @@ var config = {
   cnzz_tracker_id: '',
 
   // mongodb 配置
-  db: 'mongodb://127.0.0.1:27017/cms',
-  // db: 'mongodb://123.56.230.118:27017/photocms',
+  //db: 'mongodb://127.0.0.1:27017/cms',
+  db: 'mongodb://123.56.230.118:27017/photocms',
 
   session_secret: 'sdasd8sdujja', // 务必修改
   auth_cookie_name: 'manyingapp',
